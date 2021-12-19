9e87676801953baa884344f6d0c16555354d9855
deepenkumal <deepenkumal@noreply.github.com>
2021-12-19 20:31:14 +0545

    limiting for only post request

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ app.use("/add-product", (req, res, next) => {
   );
 });
 
-app.use("/product", (req, res, next) => {
+app.post("/product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  res.send("<h1> This is home page !</h1>");
+  res.send("<h1> This is node express !</h1>");
 });
 
 app.listen(3000);
