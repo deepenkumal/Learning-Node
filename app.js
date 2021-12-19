bf24346aeab88a0d5ab5beb52bc3d3acae087b01
deepenkumal <deepenkumal@noreply.github.com>
2021-12-19 21:09:19 +0545

    404 page not found

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ const parserBody = require("body-parser");
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(parserBody.urlencoded({ extended: false }));
 
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page not found</h1>");
+});
 
 app.listen(3000);
