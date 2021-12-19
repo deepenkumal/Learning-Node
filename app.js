6a05ad571f248bc1861a32bf7068f63ee182e91f
deepenkumal <deepenkumal@noreply.github.com>
2021-12-19 14:49:55 +0545

    Middleware in Express

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const http = require('http');
+const express = require('express');
+const app = express();
+
+app.use((req,res,next)=>{
+    console.log('Hello middleware');
+    res.write('<h1>Hello middleware!</h1>')
+    next();
+})
+
+app.use((req,res,next)=>{
+    console.log('Another middleware!!');
+    res.write('<h1>Hello Another middleware!</h1>')
+
+})
 
 // create server
-const server = http.createServer((req,res)=>{
-console.log("server started at port 3000");
-console.log(req.url);
-res.setHeader('Content-Type','txt/html')
-res.write('<h1>Hello world!!</h1>')
-});
+const server = http.createServer(app)
 server.listen(3000); //server started at port 3000;
